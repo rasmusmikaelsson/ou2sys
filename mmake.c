18a692319d542bf28f7b0803c0216c1b1d6b82b2
Rasmus Mikaelsson <ralle.mikaelsson@hotmail.com>
2025-10-03 10:51:36 +0200

    some updates

--- mmake.c
+++ mmake.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -6,14 +7,20 @@
 #define FALSE 0;
 #define TRUE 1;
 
+void handel_target(const char *target, makefile *mmakefile);
+void build_target(const char *target, char **args);
+
 #define ERORR "\e[0;31m";
 #define RESET "\e[0m";
 
 int main(int argc, char *argv[]) {
     // int silenceCommands = FALSE;
-    // int forceBuild = FALSE;
+    int forceBuild = FALSE;
     makefile *mmakefile;
+	const char **rulePrereq;
     const char *defaultTarget;
+	rule *currentRule;
+	char **args;
 
     FILE *fp;
     int opt;
@@ -23,10 +30,11 @@ int main(int argc, char *argv[]) {
             case 'f':
                 printf("Found flag 'f' and filename %s\n", optarg);
                 fp = fopen(optarg, "r");
+                printf("Opened file: %s\n", optarg);
                 break;
             case 'B':
                 printf("Found flag 'B'\n");
-                // forceBuild = TRUE;
+                forceBuild = TRUE;
                 break;
             case 's':
                 printf("Found flag 's'\n");
@@ -41,15 +49,48 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("custom makefile = TRUE\n");
     printf("%p\n",fp);
     mmakefile = parse_makefile(fp);
     if(mmakefile != NULL) {
         defaultTarget = makefile_default_target(mmakefile);
-        printf("Default target: %s", defaultTarget);
+		currentRule = makefile_rule(mmakefile, defaultTarget);
+		rulePrereq = rule_prereq(currentRule);
+		args = rule_cmd(currentRule);   
+		printf("Default target: %s\n", defaultTarget);
+
+		if(forceBuild) {
+			// run arguments given for default target 
+		} else {
+			handel_target(defaultTarget, mmakefile);
+		}
+
     } else {
         printf("Makefile = NULL\n");
     }
-
     return 0;
+}
+
+void handel_target(const char *target, makefile *mmakefile) {
+	printf("current target: %s\n", target);
+	rule *currentRule = makefile_rule(mmakefile, target);
+	char **args = rule_cmd(currentRule);
+	
+	if(currentRule == NULL) {
+		printf("last rule\n");
+		return;
+	}
+	
+	int index = 0;
+	const char **current_rule_prereq = rule_prereq(currentRule);
+	while(current_rule_prereq[index] != NULL) {
+		handel_target(current_rule_prereq[index], mmakefile);
+
+		build_target(current_rule_prereq[index], args);
+		index++;
+	}
+}
+
+void build_target(const char *target, char **args) {
+	printf("Building target: %s\n", target);
+	
 }
